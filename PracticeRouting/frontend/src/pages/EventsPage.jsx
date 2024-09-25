fd3b26c3e62f9729f6ac5cdf4929f9931572b5e3
thezeeshanhassan <thezeeshanhassan@gmail.com>
2024-09-25 23:09:23 +0500

    Axios Method Written

--- PracticeRouting/frontend/src/pages/EventsPage.jsx
+++ PracticeRouting/frontend/src/pages/EventsPage.jsx
@@ -1,3 +1,24 @@
+import { useEffect } from "react";
+import EventList from "../components/EventsList";
+const axios = require("axios");
+
+const event = [
+  {
+    id: "1",
+    title: "Event 1",
+    date: Date.now(),
+    image: "https://aba",
+  },
+];
+
 export default function EventsPage() {
-  return <></>;
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios("/");
+      console.log(response);
+    };
+    fetchData();
+  }, []);
+
+  return <EventList events={event}></EventList>;
 }
